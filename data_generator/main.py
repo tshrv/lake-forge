6c51fe952a0e8279826a43ca0e7e321b00643420
Tushar Srivastava <tusharsrivastava162@gmail.com>
2026-05-09 15:17:41 +0000

    refactor: autogenerate data dir based on scale factor

--- data_generator/main.py
+++ data_generator/main.py
@@ -2,8 +2,6 @@ import argparse
 import os
 import shutil
 import subprocess
-import time
-from typing import Optional
 
 from loguru import logger
 
@@ -14,24 +12,13 @@ from utils import timeit
 def main():
     logger.info("Data generation started")
     args = parse_arguments()
-    generate_parquet_data(
-        data_dir_name=args.data_dir_name, scale_factor=args.scale_factor
-    )
+    generate_parquet_data(scale_factor=args.scale_factor)
     logger.info("Data generation completed")
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
         description="Generate TPCH data in Parquet format."
-    )
-    parser.add_argument(
-        "--data-dir-name",
-        type=str,
-        default=None,
-        help=(
-            "Name of the output data directory. If not provided, "
-            "a unique name will be generated."
-        ),
     )
     parser.add_argument(
         "--scale-factor",
@@ -42,13 +29,12 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def generate_parquet_data(data_dir_name: Optional[str] = None, scale_factor: int = 1):
+def generate_parquet_data(scale_factor: int = 1):
     """
     Generate data using tpchgen-cli into specified output directory
     with given scale factor. The data will be generated in Parquet format.
     """
-    if not data_dir_name:
-        data_dir_name = f"data_sf_{scale_factor}_{int(time.time())}"
+    data_dir_name = f"data_sf_{scale_factor}"
     output_dir = f"./data/{data_dir_name}"
 
     # delete directory if it already exists
